7ebcafc099d7fab2c6e1a15a90271b6b6ecb44ca
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2024-12-10 02:03:55 -0300

    Feature: Listar y unirse a clases

--- estadio/roles/personal/urls.py
+++ estadio/roles/personal/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from estadio.roles.personal import views
 
 urlpatterns = [
-    
+    path('listar-clases-disponibles/', views.listar_clases_disponibles ),
 ]
--- estadio/roles/personal/views.py
+++ estadio/roles/personal/views.py
@@ -4,25 +4,35 @@ from estadio.models import Clase, Usuario, Rol
 from django.views.decorators.csrf import csrf_exempt
 
 
-def crear_usuario(request):
+def listar_clases_disponibles(request):
+    if request.method == 'GET':
+        clases = Clase.objects.all()
+        clases_list = list(clases.values('id', 'nombre', 'descripcion', 'cupo_total', 'cupo_disponible'))
+        return JsonResponse(clases_list, safe=False, status=200)
+    else:
+        return JsonResponse({'message': 'Método no permitido'}, status=405)
+    
+def unirse_clase(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body)
-            print(f'Data: {data}')
-            rolInstance = Rol.objects.get(id=data.get('rol'))
-            user = Usuario.objects.create(
+            id_clase = data.get('id_clase')
+            id_usuario = data.get('id_usuario')
+            
+            clase = Clase.objects.get(id=id_clase)
+            usuario = Usuario.objects.get(id=id_usuario)
+            
+            if clase.cupo_disponible > 0:
+                clase.cupo_disponible -= 1
+                clase.save()
                 
-                email=data.get('email'),
-                username=data.get('username'),
-                password=data.get('password'),
-                rol=rolInstance
-            )
-            
-            user.save()
-            
-            return JsonResponse({'message': 'Usuario creado correctamente'}, safe=False, status=200)
+                usuario.clases.add(clase)
+                usuario.save()
+                
+                return JsonResponse({'message': 'Usuario inscrito correctamente'}, safe=False, status=200)
+            else:
+                return JsonResponse({'message': 'No hay cupos disponibles'}, status=400)
         except Exception as e:
-            print(f"Error: {e}")
-            return JsonResponse({f'message': 'Error al crear usuario. El error es:'}, status=400)
-            
-    
+            return JsonResponse({'message': str(e)}, status=400)
+    else:
+        return JsonResponse({'message': 'Método no permitido'}, status=405)
